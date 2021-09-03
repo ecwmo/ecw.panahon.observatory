04f09bce07b29ff6ef37e4e92baf983c7d8c0916
Emilio Gozo <emil.gozo@gmail.com>
2021-09-03 17:21:15 +0800

    first commmit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,168 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="">
+    <Navbar />
+    <div id="main" class="d-flex py-5 px-4">
+      <div id="sidebar" class="d-flex flex-column gap-2">
+        <div>Map</div>
+        <div class="d-flex justify-content-evenly mb-2">
+          <button type="button" class="btn btn-light btn-xs rounded-pill">
+            Min
+          </button>
+          <button type="button" class="btn btn-secondary btn-xs rounded-pill">
+            Max
+          </button>
+        </div>
+        <div class="d-flex flex-column gap-3">
+          <button type="button" class="btn btn-light btn-square btn-sm">
+            WIND POWER
+          </button>
+          <button type="button" class="btn btn-secondary btn-square btn-sm">
+            SOLAR POWER
+          </button>
+          <button type="button" class="btn btn-secondary btn-square btn-sm">
+            TEMPERATURE
+          </button>
+          <button type="button" class="btn btn-secondary btn-square btn-sm">
+            WIND SPEED
+          </button>
+          <button type="button" class="btn btn-secondary btn-square btn-sm">
+            RAIN
+          </button>
+        </div>
+      </div>
+      <div
+        id="map"
+        class="d-flex flex-column align-items-center"
+        style="width: 400px"
+      >
+        <img alt="Map" src="./assets/map/wrf-wpd_mean.png" />
+        <img
+          alt="Colobar"
+          src="./assets/map/cmap/wrf-wpd_cmap.png"
+          style="height: 6%; width: 50%"
+        />
+      </div>
+      <div id="info" class="d-flex flex-column align-items-start flex-grow-1">
+        <span>3 Sept 2021</span>
+        <span class="fs-3 mb-3">Clean Power | Weather Forecast</span>
+        <div>
+          <select class="form-select" aria-label="Default select example">
+            <option selected>Ateneo de Manila University</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+        </div>
+        <div class="w-100">
+          <nav
+            class="nav nav-pills justify-content-evenly mt-5 fs-5 border-bottom"
+          >
+            <a
+              class="nav-link nav-btn-square bg-secondary active"
+              aria-current="page"
+              href="#"
+              >Today</a
+            >
+            <a class="nav-link text-light" href="#">Tomorrow</a>
+            <a class="nav-link text-light" href="#">Extended</a>
+          </nav>
+        </div>
+        <div class="d-flex flex-column p-3 w-100 gap-3">
+          <div class="d-flex justify-content-center gap-4 w-100">
+            <div
+              class="weather-card d-flex align-items-center py-2"
+              style="width: 16rem"
+            >
+              <div class="w-25">Icon</div>
+              <div class="w-75 d-flex flex-column align-items-center">
+                <div>WIND POWER (MW)</div>
+                <div class="d-flex align-items-end gap-2">
+                  <div class="pb-2">Ave</div>
+                  <div class="fs-1">5.4</div>
+                </div>
+                <div class="d-flex align-items-end gap-2">
+                  <div class="pb-2">Max</div>
+                  <div class="fs-1">8.3</div>
+                </div>
+              </div>
+            </div>
+            <div
+              class="weather-card d-flex align-items-center py-2"
+              style="width: 16rem"
+            >
+              <div class="w-25">Icon</div>
+              <div class="w-75 d-flex flex-column align-items-center">
+                <div>SOLAR POWER (MW)</div>
+                <div class="d-flex align-items-end gap-2">
+                  <div class="pb-2">Ave</div>
+                  <div class="fs-1">2.3</div>
+                </div>
+                <div class="d-flex align-items-end gap-2">
+                  <div class="pb-2">Max</div>
+                  <div class="fs-1">8.3</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="d-flex justify-content-center gap-4 w-100">
+            <div
+              class="weather-card d-flex align-items-center py-2"
+              style="width: 16rem"
+            >
+              <div class="w-25">Icon</div>
+              <div class="w-75 d-flex flex-column align-items-center">
+                <div>TEMPERATURE (°C)</div>
+                <div class="d-flex align-items-end gap-2">
+                  <div class="pb-2">Min</div>
+                  <div class="fs-1">28</div>
+                </div>
+                <div class="d-flex align-items-end gap-2">
+                  <div class="pb-2">Max</div>
+                  <div class="fs-1">37</div>
+                </div>
+              </div>
+            </div>
+            <div
+              class="weather-card d-flex align-items-center py-2"
+              style="width: 16rem"
+            >
+              <div class="w-25">Icon</div>
+              <div class="w-75 d-flex flex-column align-items-center">
+                <div>WIND SPEED (kph)</div>
+                <div class="d-flex align-items-end gap-2">
+                  <div class="pb-2">Min</div>
+                  <div class="fs-1">3</div>
+                </div>
+                <div class="d-flex align-items-end gap-2">
+                  <div class="pb-2">Max</div>
+                  <div class="fs-1">14</div>
+                </div>
+              </div>
+            </div>
+            <div
+              class="weather-card d-flex align-items-center py-2"
+              style="width: 16rem"
+            >
+              <div class="w-25">Icon</div>
+              <div class="w-75 d-flex flex-column align-items-center">
+                <div>RAIN CHANCE</div>
+                <div class="fs-1">LOW</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Navbar,
   },
 };
 </script>
@@ -20,7 +173,28 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #212529;
+}
+
+#main {
+  background-color: #212529;
+  color: #e9ecef;
+}
+
+.btn.btn-xs {
+  padding: 0.1rem 0.75rem;
+  font-size: 0.75rem;
+}
+
+.btn.btn-square {
+  border-radius: 0;
+}
+
+.nav-pills .nav-link.nav-btn-square {
+  border-radius: 0;
+}
+
+.weather-card {
+  background-color: #495057;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Manila Observatory</a>
+        <div>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"
+                  >Weather</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Acknowledgements</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Live Weather Data</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
