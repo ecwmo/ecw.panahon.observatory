98963ff87f46a86c7064d3b4757e1fb4f1f69bbd
Emilio Gozo <emil.gozo@gmail.com>
2021-09-21 13:27:06 +0800

    style: prevent selecting text

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col w-full h-screen">
+  <div class="flex flex-col w-full h-screen select-none">
     <Navbar />
     <div class="w-full flex flex-col p-4 md:p-6 bg-gray-700 text-gray-200">
       <router-view />
