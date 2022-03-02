ce0ac51e56b07142e8a6dc98d5b604798cae0fb1
Emilio Gozo <emil.gozo@gmail.com>
2022-03-02 13:53:39 +0800

    feat: remove forecastImgVariants, rename props

--- src/composables/useForecastImg.ts
+++ src/composables/useForecastImg.ts
@@ -1,48 +1,32 @@
 import { computed, ref, Ref } from 'vue'
 
-export interface ForecastImg {
-  forecastImgVariants: Ref<string[]>
-  forecastImg: Ref<string>
-  forecastImgCmap: Ref<string>
-}
-
 const useForecastImg = (
-  activeVariable: Ref<string>,
-  activeDay = ref(0),
-  activeImgType = ref(0)
-): ForecastImg => {
-  const forecastImgVariants = computed(() => {
-    if (['wpd', 'ppv'].indexOf(activeVariable.value) !== -1)
-      return ['Ave', 'Max']
-    else if (activeVariable.value === 'rain') return []
-    else return ['Min', 'Max']
-  })
-
+  variable: Ref<string>,
+  day = ref(0),
+  imgType = ref(0)
+) => {
   const forecastImg = computed(() => {
     let imgTypes: string[] | string = ['min', 'max']
-    if (['wpd', 'ppv'].indexOf(activeVariable.value) !== -1)
+    if (['wpd', 'ppv'].indexOf(variable.value) !== -1)
       imgTypes = ['mean', 'max']
-    else if (activeVariable.value === 'rain') imgTypes = ''
+    else if (variable.value === 'rain') imgTypes = ''
 
-    const hh = activeDay.value < 2 ? (activeDay.value + 1) * 24 : 24
-    const varName =
-      activeVariable.value !== 'rain' ? activeVariable.value : 'rainchance'
+    const hh = day.value < 2 ? (day.value + 1) * 24 : 24
+    const varName = variable.value !== 'rain' ? variable.value : 'rainchance'
 
-    if (imgTypes === '')
+    if (variable.value === 'rain')
       return `https://panahon.observatory.ph/resources/model/web_img/wrf-${hh}hr_${varName}_latest.png`
     return `https://panahon.observatory.ph/resources/model/web_img/wrf-${hh}hr_${varName}_${
-      imgTypes[activeImgType.value]
+      imgTypes[imgType.value]
     }_latest.png`
   })
 
   const forecastImgCmap = computed(() => {
-    const varName =
-      activeVariable.value !== 'rain' ? activeVariable.value : 'rainchance'
+    const varName = variable.value !== 'rain' ? variable.value : 'rainchance'
     return `https://panahon.observatory.ph/resources/model/web_img/cmap/wrf-${varName}_cmap.png`
   })
 
   return {
-    forecastImgVariants,
     forecastImg,
     forecastImgCmap,
   }
