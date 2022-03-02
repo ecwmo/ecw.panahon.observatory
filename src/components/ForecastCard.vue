fcd80e1f10d9841962de8369db57b19665f9f222
Emilio Gozo <emil.gozo@gmail.com>
2022-03-02 13:51:04 +0800

    test: FOrecastCard

--- src/components/ForecastCard.vue
+++ src/components/ForecastCard.vue
@@ -1,15 +1,21 @@
 <template>
   <a
     href="#"
-    class="flex items-center p-3 md:p-4 w-56 h-32 md:w-60"
+    class="flex items-center p-3 md:p-4 w-56 h-32 md:w-60 cursor-default"
     @click.prevent
+    data-test="card"
   >
-    <div class="w-1/5 flex justify-center">
+    <div class="w-1/5 flex justify-center" data-test="card-icon">
       <img :src="icon" :class="[name === 'temp' ? 'w-3/4' : '']" />
     </div>
     <div class="w-4/5 flex-grow flex flex-col items-center">
-      <span>{{ title }}</span>
-      <div v-for="d in data" :key="d.name" class="flex items-end gap-2">
+      <span data-test="card-title">{{ title }}</span>
+      <div
+        v-for="d in data"
+        :key="d.name"
+        class="flex items-end gap-2"
+        data-test="card-body"
+      >
         <div class="textxs md:text-sm pb-1">{{ d.name }}</div>
         <div class="text-3xl md:text-4xl font-bold uppercase">
           {{ d.value }}
