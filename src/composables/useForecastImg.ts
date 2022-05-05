ae1ebf39c2c01d3062a1fff4a22833feaa0330f7
Emilio Gozo <emil.gozo@gmail.com>
2022-05-05 14:33:31 +0800

    feat: get images from api

--- src/composables/useForecastImg.ts
+++ src/composables/useForecastImg.ts
@@ -1,10 +1,19 @@
-import { computed, ref, Ref } from 'vue'
+import { computed, ref, onMounted } from 'vue'
+import axios from 'axios'
 
-const useForecastImg = (
-  variable: Ref<string>,
-  day = ref(0),
-  imgType = ref(0)
-) => {
+interface ForecastImgs {
+  imgs: string[]
+  cmaps: string[]
+}
+
+const useForecastImg = (variable = ref(''), day = ref(0), imgType = ref(0)) => {
+  const baseUrl = 'https://panahon.observatory.ph'
+  const forecastImgs = ref(<ForecastImgs>{})
+
+  const varName = computed(() =>
+    variable.value !== 'rain' ? variable.value : 'rainchance'
+  )
+
   const forecastImg = computed(() => {
     let imgTypes: string[] | string = ['min', 'max']
     if (['wpd', 'ppv'].indexOf(variable.value) !== -1)
@@ -12,18 +21,28 @@ const useForecastImg = (
     else if (variable.value === 'rain') imgTypes = ''
 
     const hh = day.value < 2 ? (day.value + 1) * 24 : 24
-    const varName = variable.value !== 'rain' ? variable.value : 'rainchance'
 
-    if (variable.value === 'rain')
-      return `https://panahon.observatory.ph/resources/model/web_img/wrf-${hh}hr_${varName}_latest.png`
-    return `https://panahon.observatory.ph/resources/model/web_img/wrf-${hh}hr_${varName}_${
-      imgTypes[imgType.value]
-    }_latest.png`
+    const img = forecastImgs.value?.imgs?.find((i) =>
+      i.includes(
+        variable.value !== 'rain'
+          ? `${hh}hr_${varName.value}_${imgTypes[imgType.value]}`
+          : `${hh}hr_${varName.value}`
+      )
+    )
+    return `${baseUrl}/${img}`
   })
 
   const forecastImgCmap = computed(() => {
-    const varName = variable.value !== 'rain' ? variable.value : 'rainchance'
-    return `https://panahon.observatory.ph/resources/model/web_img/cmap/wrf-${varName}_cmap.png`
+    const img = forecastImgs.value?.cmaps?.find((i) =>
+      i.includes(varName.value)
+    )
+    return `${baseUrl}/${img}`
+  })
+
+  onMounted(async () => {
+    forecastImgs.value = await axios
+      .get(`${baseUrl}/api/forecast-img.php`)
+      .then(({ data }) => <ForecastImgs>data)
   })
 
   return {
